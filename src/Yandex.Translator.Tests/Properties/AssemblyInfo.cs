a1251ddf987829c777e0dbce473d092c6b7ea0b6
prokhor-ozornin <prokhor.ozornin@yandex.ru>
2015-08-12 12:04:48 +0500

    1. Версия изменена на 1.4. 2. Добавлена поддержка .NET Framework 4.6

---
+++ src/Yandex.Translator.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Yandex.Translator.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Prokhor Ozornin <prokhor.ozornin@yandex.ru>")]
+[assembly: AssemblyProduct("Yandex.Translator.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2014 - 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b79f85aa-c61f-48d7-8969-09c9ca85e23e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.3.1")]
+[assembly: AssemblyFileVersion("1.3.1")]
+
+[assembly: CLSCompliant(true)]
